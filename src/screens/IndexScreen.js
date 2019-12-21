df34c190196373a7a1be8dd2f25048519e32614b
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-21 15:29:19 +1000

    add a trash icon and some basic style to the blog post

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
@@ -12,7 +13,10 @@ const IndexScreen = () => {
         keyExtractor={(blogPost) => blogPost.title }
         renderItem={({ item }) => {
           return (
-            <Text>{item.title}</Text>
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather style={styles.icon} name='trash' />
+            </View>
           );
         }}
       />
@@ -24,6 +28,17 @@ const IndexScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 
 export default IndexScreen;
