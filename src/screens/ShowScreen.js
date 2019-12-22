a9256dce74c6c74cf516c389e15d8341edbaa7f3
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-22 17:10:52 +1000

    touch up

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,16 +1,16 @@
 import React, { useContext } from 'react';
-import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
   const id = navigation.getParam('id');
   const { state } = useContext(Context);
-  const blog = state.find(blog => blog.id === id);
+  const blogPost = state.find(blogPost => blogPost.id === id);
 
   return (
     <View>
-      <Text>Show Screen for {blog.title} {blog.id}</Text>
+      <Text>Show Screen for {blogPost.title} {blogPost.id}</Text>
     </View>
   );
 };
